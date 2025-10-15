224ee0b11ba6831732744b817cfd84b0791fbbee
Mamali <mohammadrezaghorbani997@gmail.com>
2025-10-15 20:23:34 +0330

    10/15/2025

---
+++ swiper/js swiper/script-dasti.js
@@ -0,0 +1,11 @@
+var swiper = new Swiper(".my1Swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".my2Swiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
